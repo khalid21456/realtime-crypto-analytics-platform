7e0129c8e90f63baca487a92c990856262a6946c
Souilimaa <somamy19@gmail.com>
2025-12-17 04:37:34 +0100

    add docker file

--- consumer-service/src/main/java/ma/ensam/consumer_service/services/ConsumerService.java
+++ consumer-service/src/main/java/ma/ensam/consumer_service/services/ConsumerService.java
@@ -3,65 +3,43 @@ package ma.ensam.consumer_service.services;
 import ma.ensam.consumer_service.domaine.DTO.CryptoDTO;
 import ma.ensam.consumer_service.domaine.entities.Crypto;
 import ma.ensam.consumer_service.repositories.CryptoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Service;
+import org.springframework.context.annotation.Configuration;
+
 import java.time.Instant;
 import java.util.Date;
 import java.util.function.Consumer;
-@SuppressWarnings("unused")
 
-@Service
+@Configuration
 public class ConsumerService {
 
-    @Autowired
-    private StreamBridge streamBridge;
+    private final CryptoRepository cryptoRepository;
 
-    @Autowired
-    private CryptoRepository cryptoRepository;
-
-   // @Bean
-//    public Consumer<CryptoDTO> cryptoConsumer() {
-//        return (input) -> {
-//            System.out.println(input.getPrice());
-//            Crypto crypto = Crypto.builder().size(Double.parseDouble(input.getSize()))
-//                    .ask(Double.parseDouble(input.getAsk()))
-//                    .pair(input.getPair())
-//                    .bid(Double.parseDouble(input.getBid()))
-//                    .volume(Double.parseDouble(input.getVolume()))
-//                    .rfq_volume(Double.parseDouble(input.getRfq_volume()))
-//                    .time(new Date(input.getTime()))
-//                    .price(Double.parseDouble(input.getPrice()))
-//                    .trade_id(input.getTrade_id())
-//                    .build();
-//
-//            cryptoRepository.save(crypto);
-//        };
-//    }
+    public ConsumerService(CryptoRepository cryptoRepository) {
+        this.cryptoRepository = cryptoRepository;
+    }
 
     @Bean
     public Consumer<CryptoDTO> cryptoConsumer() {
-        return (input) -> {
-            System.out.println("rfq_volume :"+input.getRfq_volume());
+        return input -> {
             Crypto crypto = Crypto.builder()
                     .size(safeDouble(input.getSize()))
                     .ask(safeDouble(input.getAsk()))
                     .pair(input.getPair())
                     .bid(safeDouble(input.getBid()))
                     .volume(safeDouble(input.getVolume()))
-                    .rfq_volume(Double.valueOf(input.getRfq_volume()))
+                    .rfq_volume(safeDouble(input.getRfq_volume()))
                     .time(Date.from(Instant.parse(input.getTime())))
                     .price(safeDouble(input.getPrice()))
                     .trade_id(input.getTrade_id())
                     .build();
 
-            //cryptoRepository.save(crypto);
+            cryptoRepository.save(crypto);
         };
     }
 
     private Double safeDouble(String value) {
         try { return Double.parseDouble(value); }
-        catch (Exception e) { return null;}
+        catch (Exception e) { return null; }
     }
 }
